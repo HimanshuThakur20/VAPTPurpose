29ecc18ba8fae0344fa4e4383604d38f55599bb8
Himanshu <72663195+HimanshuThakur20@users.noreply.github.com>
2025-03-25 18:16:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ alert(4*4*4);
 alert("Cookies: " + document.cookie);
 // Set background color to red
 document.body.style.backgroundColor = "red";
+document.querySelectorAll("div").forEach(div => {
+    div.style.backgroundColor = "red";
+});
