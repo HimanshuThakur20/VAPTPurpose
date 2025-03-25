c788744138b86c4a5014ae08523ff75384aea7f7
Himanshu <72663195+HimanshuThakur20@users.noreply.github.com>
2025-03-25 18:14:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 alert("cross domain script execution done ");
 alert(4*4*4);
 alert("Cookies: " + document.cookie);
-'><b/style=position:fixed;top:0;left:0;font-size:200px>XSS<!--
+// Set background color to red
+document.body.style.backgroundColor = "red";
