50e8a6327d3050aa6fa010ac777e625689355ed8
Himanshu <72663195+HimanshuThakur20@users.noreply.github.com>
2025-03-25 18:32:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ alert("cross domain script execution done ");
 alert("Cookies: " + document.cookie);
 // Set background color to red
 // document.body.style.backgroundColor = "red";
-// document.querySelectorAll("div").forEach(div => {
-//     div.style.backgroundColor = "red";
-// });
+document.querySelectorAll("div").forEach(div => {
+    div.style.backgroundColor = "red";
+});
 // const cookies = document.cookie;
 // const blob = new Blob([cookies], { type: "text/plain" });
 // const a = document.createElement("a");
